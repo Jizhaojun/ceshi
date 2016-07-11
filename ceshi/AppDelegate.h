fa6e900dc7f1becd8c5281a02e1bb13cc8ce9276
Jizhaojun <jnn@jnndeiMac.local>
2016-07-11 08:38:02 +0800

    ceshill
    初始化测试

---
+++ ceshi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ceshi
+//
+//  Created by jnn on 16/7/11.
+//  Copyright © 2016年 纪妞妞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ceshi/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ceshi
+//
+//  Created by jnn on 16/7/11.
+//  Copyright © 2016年 纪妞妞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
